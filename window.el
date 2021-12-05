894a9f224644e0ba07521deece22245cf095e913
Justin Silverman <jsilve24@gmail.com>
2021-12-05 15:12:45 -0500

    Fixing org agenda files and split-window-sensibly thresholds

--- window.el
+++ window.el
@@ -20,13 +20,48 @@
 ;;; Code:
 
 ;;;###autoload
+(defun split-window-sensibly-prefer-horizontal (&optional window)
+"Based on split-window-sensibly, but designed to prefer a horizontal split,
+i.e. windows tiled side-by-side."
+  (let ((window (or window (selected-window))))
+    (or (and (window-splittable-p window t)
+         ;; Split window horizontally
+         (with-selected-window window
+           (split-window-right)))
+    (and (window-splittable-p window)
+         ;; Split window vertically
+         (with-selected-window window
+           (split-window-below)))
+    (and
+         ;; If WINDOW is the only usable window on its frame (it is
+         ;; the only one or, not being the only one, all the other
+         ;; ones are dedicated) and is not the minibuffer window, try
+         ;; to split it horizontally disregarding the value of
+         ;; `split-height-threshold'.
+         (let ((frame (window-frame window)))
+           (or
+            (eq window (frame-root-window frame))
+            (catch 'done
+              (walk-window-tree (lambda (w)
+                                  (unless (or (eq w window)
+                                              (window-dedicated-p w))
+                                    (throw 'done nil)))
+                                frame)
+              t)))
+     (not (window-minibuffer-p window))
+     (let ((split-width-threshold 0))
+       (when (window-splittable-p window t)
+         (with-selected-window window
+               (split-window-right))))))))
+
+;;;###autoload
 (defun jds~new-frame-or-new-window ()
   "New Frame and Focus unless using EXWM then new window."
-    (if (frame-parameter (selected-frame) 'exwm-active)
+  (if (frame-parameter (selected-frame) 'exwm-active)
       (progn
-	(let ((split-width-threshold 20)
+	(let ((split-width-threshold 150)
 	      (split-height-threshold 20))
-	  (if (not (split-window-sensibly))
+	  (if (not (split-window-sensibly-prefer-horizontal))
 	      (split-window-right)))
 	(call-interactively #'other-window))
     (select-frame (make-frame))))
