590c44c0d3687563c202cfc28ccb8e33e2b1c271
Justin Silverman <jsilve24@gmail.com>
2022-01-20 13:12:18 -0500

    Added hooks to run calendar fetching after init

--- calendar.el
+++ calendar.el
@@ -6,5 +6,13 @@
   (load-file "~/.org-caldav-secrets.el.gpg")
   (setq org-gcal-auto-archive nil))
 
+(add-hook 'after-init-hook #'org-gcal-fetch)
+
+;;;###autoload
+(defun jds~async-exchange-calendar-fetch ()
+  (interactive)
+  (jds/quiet-async-shell-commands "~/bin/get_exchange_cal.sh"))
+(add-hook 'after-init-hook #'org-gcal-fetch)
+
 (provide 'calendar)
 ;;; calendar.el ends here
