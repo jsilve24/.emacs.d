3c16aba5f28e11345f45c5344bb1b6c1f3cb4500
Justin Silverman <jsilve24@gmail.com>
2022-02-24 09:38:27 -0500

    fixing citar to allow multiple citations again

--- latex.el
+++ latex.el
@@ -174,25 +174,27 @@
   :straight t
   :defer t
   :config
+  (setq citar-select-multiple t)
+  
   ;; watch for changes in bib files
   (citar-filenotify-setup '(LaTeX-mode-hook org-mode-hook))
 
   ;; nice file icons
   (setq citar-symbols
-  `((file . (,(all-the-icons-icon-for-file "foo.pdf" :face 'all-the-icons-dred) .
-            ,(all-the-icons-icon-for-file "foo.pdf" :face 'citar-icon-dim)))
-    (note . (,(all-the-icons-icon-for-file "foo.txt") .
-            ,(all-the-icons-icon-for-file "foo.txt" :face 'citar-icon-dim)))
-    (link .
-        (,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'all-the-icons-dpurple) .
-        ,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'citar-icon-dim)))))
-;; Here we define a face to dim non 'active' icons, but preserve alignment
-(defface citar-icon-dim
+	`((file . (,(all-the-icons-icon-for-file "foo.pdf" :face 'all-the-icons-dred) .
+		   ,(all-the-icons-icon-for-file "foo.pdf" :face 'citar-icon-dim)))
+	  (note . (,(all-the-icons-icon-for-file "foo.txt") .
+		   ,(all-the-icons-icon-for-file "foo.txt" :face 'citar-icon-dim)))
+	  (link .
+		(,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'all-the-icons-dpurple) .
+		 ,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'citar-icon-dim)))))
+  ;; Here we define a face to dim non 'active' icons, but preserve alignment
+  (defface citar-icon-dim
     '((((background dark)) :foreground "#282c34")
-     (((background light)) :foreground "#fafafa"))
-     "Face for obscuring/dimming icons"
-     :group 'all-the-icons-faces)
-)
+      (((background light)) :foreground "#fafafa"))
+    "Face for obscuring/dimming icons"
+    :group 'all-the-icons-faces)
+  )
 
 
 ;;; setup biblio
