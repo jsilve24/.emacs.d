3d351159e1131712e4cece8a3823575436787ded
Justin Silverman <jsilve24@gmail.com>
2022-05-23 17:09:24 -0400

    removed org-gcal

--- calendar.el
+++ calendar.el
@@ -1,17 +1,28 @@
 ;;; calendar.el --- calendar config -*- lexical-binding: t; -*-
 
-(use-package org-gcal
-  :commands (org-gcal-sync org-gcal-fetch org-gcal-post-at-point org-gcal-request-token org-gcal-delete-at-point)
-  :config
-  (load-file "~/.org-caldav-secrets.el.gpg")
-  (setq org-gcal-auto-archive nil))
-(add-hook 'after-init-hook #'org-gcal-fetch)
+;; (use-package org-gcal
+;;   :commands (org-gcal-sync org-gcal-fetch org-gcal-post-at-point org-gcal-request-token org-gcal-delete-at-point)
+;;   :config
+;;   (load-file "~/.org-caldav-secrets.el.gpg")
+;;   ;; (setq org-gcal-auto-archive nil)
+;;   )
+;; (add-hook 'after-init-hook #'org-gcal-fetch)
 
 ;;;###autoload
 (defun jds/async-exchange-calendar-fetch ()
   (interactive)
   (jds/quiet-async-shell-commands "~/bin/get_exchange_cal.sh"))
 (add-hook 'after-init-hook #'jds/async-exchange-calendar-fetch)
+
+;;;###autoload
+(defun jds/async-google-calendar-fetch ()
+  (interactive)
+  (jds/quiet-async-shell-commands "~/bin/get_gcal.sh"))
+(add-hook 'after-init-hook #'jds/async-google-calendar-fetch)
+
+
+
+
 
 (provide 'calendar)
 ;;; calendar.el ends here
--- pdf.el
+++ pdf.el
@@ -36,13 +36,13 @@
   ;; setup printing
   ;; (setq lpr-command "gtklp")
   ;; (setq ps-lpr-command "gtklp")
-  
+
   ;; see cups help page for lots on how to print with lpr and lp
   (setq pdf-misc-print-program-executable "/usr/bin/lpr"
 	pdf-misc-print-program-args (list "-o sides=two-sided-long-edge"
 					  ;; "-o fit-to-page"
 					  ))
-  
+
   ;; load my own version of evil-collection-pdf setup that doesn't have
   ;; the SPC binding.
   (evil-collection-pdf-setup)
@@ -50,7 +50,26 @@
   ;; unbind SPC from pdf-view-mode-map
   (general-define-key
    :keymaps 'pdf-view-mode-map
-   "SPC" nil))
+   "SPC" nil)
+
+  (defun pdf-misc-print-document (filename &optional interactive-p)
+    "Print the PDF doc FILENAME.
+
+`pdf-misc-print-program' handles the print program, which see for
+definition of INTERACTIVE-P."
+    (interactive
+     (list (pdf-view-buffer-file-name) t))
+    (cl-check-type filename (and string (satisfies file-readable-p)))
+    (let* ((program pdf-misc-print-program-executable)
+	   (args (string-join (append pdf-misc-print-program-args (list filename)) " "))
+	   (cmd (string-join (list program args) " ")))
+      ;; (message args)
+      (unless program
+	(error "No print program available"))
+      (start-process-shell-command "printing" nil cmd)
+      ;; (apply #'start-process "printing" nil program args)
+      (message "Print job started: %s %s"
+	       program args))))
 
 
 ;;; print-helper -- not enough to make a stand-alone package
@@ -61,15 +80,15 @@
   (let* ((printers (shell-command-to-string "lpstat -p -d"))
 	 (printers (split-string printers "\n"))
 	 (default (-filter (lambda (x) (string-match "default destination" x))
-		   printers))
+			   printers))
 	 (printers (-filter (lambda (x) (string-match "printer " x))
-		    printers))
+			    printers))
 	 (default (last (split-string (car default) " ")))
 	 (printers (-map (lambda (x) (nth 1 (split-string x " ")))
 			 printers))
 	 ;; move to front of list
 	 (printers (cons (car default) (remove (car default) printers))))
-	 printers))
+    printers))
 
 
 ;;;###autoload
@@ -80,13 +99,12 @@
   printer to use instead assumes that
   `pdf-misc-print-program-executable' is already set."
   (interactive "P")
-  (if arg 
+  (if arg
       (let* ((printer (completing-read "Choose a printer:" (ph--get-list-of-priters)))
-	    (pdf-misc-print-program-args
-	     ;; should this not have a space after P?
-	     ;; (add-to-list  'pdf-misc-print-program-args (concat "-P " printer))
-	     (append pdf-misc-print-program-args (list (concat "-P " printer)))
-	     ))
+	     (pdf-misc-print-program-args
+	      ;; should this not have a space after P?
+	      ;; (add-to-list  'pdf-misc-print-program-args (concat "-P " printer))
+	      (append pdf-misc-print-program-args (list (concat "-P " printer)))))
 	(pdf-misc-print-document
 	 (pdf-view-buffer-file-name)
 	 ;; dont' prompt for program to print with (nil)
@@ -97,25 +115,6 @@
      ;; dont' prompt for program to print with (nil)
      nil)))
 
-;;;###autoload
-(defun pdf-misc-print-document (filename &optional interactive-p)
-  "Print the PDF doc FILENAME.
-
-`pdf-misc-print-program' handles the print program, which see for
-definition of INTERACTIVE-P."
-  (interactive
-   (list (pdf-view-buffer-file-name) t))
-  (cl-check-type filename (and string (satisfies file-readable-p)))
-  (let* ((program pdf-misc-print-program-executable)
-        (args (string-join (append pdf-misc-print-program-args (list filename)) " "))
-	(cmd (string-join (list program args) " ")))
-    ;; (message args)
-    (unless program
-      (error "No print program available"))
-    (start-process-shell-command "printing" nil cmd)
-    ;; (apply #'start-process "printing" nil program args)
-    (message "Print job started: %s %s"
-             program args)))
 
 
 (jds/localleader-def
