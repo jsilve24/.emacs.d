2220a21571c9ba21bf42da2063af3517edc30dc5
Justin Silverman <jsilve24@gmail.com>
2022-09-19 10:51:39 -0400

    slight modifications to the display buffer ess settings

--- ess.el
+++ ess.el
@@ -60,7 +60,9 @@
 				       (reusable-frames . nil)))
 
   (add-to-list 'display-buffer-alist '("^\\*R"
-				       (display-buffer-reuse-window display-buffer-at-bottom)
+				       (display-buffer-reuse-window display-buffer-in-side-window)
+				       (side . right)
+				       (slot . -1)
 				       (window-width . 0.5)
 				       (reusable-frames . nil)))
 
@@ -74,7 +76,7 @@
   (add-to-list 'display-buffer-alist '("^R_x11"
 				       (display-buffer-reuse-window display-buffer-in-side-window)
 				       (side . right)
-				       (slot . -1)
+				       (slot . 1)
 				       (window-width . 0.33)
 				       (reusable-frames . nil))))
 
