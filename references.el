2081149f80b95b271e1f0b0c1620155c139992d1
Justin Silverman <jsilve24@gmail.com>
2022-09-23 20:46:23 -0400

    more updates to org-roam-bibtex

--- references.el
+++ references.el
@@ -46,6 +46,7 @@
 
 ;;; setup ebib -----------------------------------------------------------------
 
+(straight-use-package '(ebib :includes (ebib org-ebib)))
 (use-package ebib
   :config
   (setq ebib-preload-bib-files org-cite-global-bibliography)
@@ -57,15 +58,41 @@
 	'(("pdf" . nil)
 	  ("ps" . nil)))
   ;; bigger index window at start
-  (setq ebib-index-window-size 20))
+  (setq ebib-index-window-size 20)
+  (require 'org-ebib))
+
+;;;###autoload
+(defun jds/citar-org-roam--create-capture-note (citekey entry)
+    "Open or create org-roam node for CITEKEY and ENTRY."
+   ;; adapted from https://jethrokuan.github.io/org-roam-guide/#orgc48eb0d
+    (let ((title (citar-format--entry
+                   citar-org-roam-note-title-template entry)))
+     (org-roam-capture-
+      :templates
+      '(("r" "reference" plain "%?" :if-new
+         (file+head
+          "%(concat
+ (when citar-org-roam-subdir (concat citar-org-roam-subdir \"/\")) \"${citekey}.org\")"
+          "#+title: ${title}\n Ebib:%a\n")
+         :immediate-finish t
+         :unnarrowed t))
+      :info (list :citekey citekey)
+      :node (org-roam-node-create :title title)
+      :props '(:finalize find-file))
+     (org-roam-ref-add (concat "@" citekey))))
+
+;; (with-eval-after-load 'citar-org-roam
+;;   (setq citar-org-roam-notes-config (plist-put citar-org-roam-notes-config :create #'jds/citar-org-roam--create-capture-note)))
 
 ;; setup with org-roam notes
 ;;;###autoload
 (defun jds/ebib-popup-note (key)
-    (interactive (list (ebib--get-key-at-point)))
-    (citar-org-roam--create-capture-note key key)
-    ;; (orb-edit-note key) ;; didn't like this template, gave terrible file names
-    )
+  (interactive (list (ebib--get-key-at-point)))
+  (org-ebib-store-link)
+  (jds/citar-org-roam--create-capture-note key key)
+  (org-ebib-insert-link)
+  ;; (orb-edit-note key) ;; didn't like this template, gave terrible file names
+  )
 
 ;; override ebib-popup-note to use org-roam-bibtex
 (general-def
