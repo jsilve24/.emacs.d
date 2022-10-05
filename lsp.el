2c3f49b3613c190e486bb7fc5123561f0e5c2b6b
Justin Silverman <jsilve24@gmail.com>
2022-10-05 09:17:04 -0400

    fix(lsp): eldoc STFU!

--- lsp.el
+++ lsp.el
@@ -2,7 +2,11 @@
 
 
 (use-package eglot
-  :hook (latex-mode . eglot-ensure))
+  :hook ((latex-mode . eglot-ensure)
+	 (LaTeX-mode . eglot-ensure))
+  :config
+  (setq eldoc-echo-area-use-multiline-p 1)
+  (setq eglot-ignored-server-capabilites nil))
 
 
 (use-package consult-eglot)
