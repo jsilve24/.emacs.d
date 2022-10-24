cf0f6021c0916247eeef6462098167ec105f9656
Justin Silverman <jsilve24@gmail.com>
2022-10-24 17:18:09 -0400

    refactor: cleaning up useless text in config

--- themes.el
+++ themes.el
@@ -1,25 +1,5 @@
 ;;; themes.el --- theme related config -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2021 Justin Silverman
-;;
-;; Author: Justin Silverman <https://github.com/jsilve24>
-;; Maintainer: Justin Silverman <jds6696@psu.edu>
-;; Created: October 21, 2021
-;; Modified: October 21, 2021
-;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/jsilve24/themes
-;; Package-Requires: ((emacs "24.3"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;;  Theme related config
-;;
-;;; Code:
 
-;;; Fonts
 
 ;;; Font stuff
 
@@ -109,8 +89,6 @@
   (add-hook 'dired-mode-hook #'hl-line-mode)
   (add-hook 'org-agenda-mode-hook #'hl-line-mode)
   (add-hook 'ibuffer-mode-hook #'hl-line-mode))
-
-
 
 
 ;;; modline
