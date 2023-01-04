4bbe09108f63c04c9cdebb8c888e72bde7c53aef
Justin Silverman <jsilve24@gmail.com>
2023-01-04 10:25:57 -0500

    change: corfu now preselect first

--- capf.el
+++ capf.el
@@ -20,7 +20,7 @@
   ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
   (corfu-preview-current t) ;; Disable current candidate preview
-  (corfu-preselect-first nil)    ;; Disable candidate preselection
+  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
   ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
   ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
   ;; (corfu-scroll-margin 5)        ;; Use scroll margin
