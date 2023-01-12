2c5d40bddd8759f8d8345518fc52eb4ead50b9dd
Justin Silverman <jsilve24@gmail.com>
2023-01-12 09:51:21 -0500

    fix: better default for org-src special edit buffer window placement

--- org.el
+++ org.el
@@ -299,7 +299,7 @@
   (setq org-confirm-babel-evaluate nil)
 
   ;; better org-src buffer window placement
-  (setq org-src-window-setup 'plain)
+  (setq org-src-window-setup 'split-window-right)
 
   ;; allow binding in oeg export 
   ;; https://tex.stackexchange.com/questions/637386/org-mode-export-bold-text-to-beamer
